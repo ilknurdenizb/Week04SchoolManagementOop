2231633fba0c7e461a29ce8de471310e090a9b9c
ilknurdenizb <ideniz.baloglu@gmail.com>
2025-07-19 15:00:51 +0300

    Add project files.

---
+++ SchoolPersonMainOop/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolPersonMainOop // SchoolPersonMainOop namespace
+{
+  internal class Person  // Person class within the SchoolPersonMainOop namespace
+    {
+
+        public string Name { get; set; }  // Property for the person's name
+        public string Surname { get; set; }  // Property for the person's surname
+        public DateTime BirthDate { get; set; }  // Property for the person's birth date
+        public string Role { get; set; }  // Property for the person's role (e.g., Student, Teacher)
+        public void SelfPromote()  // Method to introduce the person
+        {
+            Console.WriteLine($"{Name} {Surname}, {Role.ToLower()} and born on {BirthDate.ToShortDateString()}, is introducing themselves.");
+        }
+
+      
+        }
+    }
---
+++ SchoolPersonMainOop/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program // Main class for the program
+{
+    static void Main(string[] args) // Main method to run the program
+    {
+        SchoolPersonMainOop.Person student1 = new SchoolPersonMainOop.Person  // Create a new student object
+        {
+            Name = "Johnny", // Set the student's name
+            Surname = "Depp",  // Set the student's surname
+            BirthDate = new DateTime(1963, 6, 9),  // Set the student's birth date
+            Role = "Student"  //Set the student's role
+        };
+
+        SchoolPersonMainOop.Person student2 = new SchoolPersonMainOop.Person  // Create another student object
+        {
+            Name = "Emma",
+            Surname = "Watson",
+            BirthDate = new DateTime(1990, 4, 15),
+            Role = "Student"
+        };
+
+        SchoolPersonMainOop.Person teacher1 = new SchoolPersonMainOop.Person
+        {
+            Name = "John",
+            Surname = "Smith",
+            BirthDate = new DateTime(1989, 3, 25),
+            Role = "Teacher"
+        };
+
+        SchoolPersonMainOop.Person teacher2 = new SchoolPersonMainOop.Person
+        {
+            Name = "Jane",
+            Surname = "Doe",
+            BirthDate = new DateTime(1985, 11, 30),
+            Role = "Teacher"
+        };
+
+        student1.SelfPromote(); // Call the SelfPromote method for student1
+        student2.SelfPromote(); // Call the SelfPromote method for student2
+        teacher1.SelfPromote(); // Call the SelfPromote method for teacher1
+        teacher2.SelfPromote(); // Call the SelfPromote method for teacher2
+    }
+}
